28977b927ba07a097b6e9b8695e1fe3ce17d248c
maks-1987 <max.moderation@gmail.com>
2017-10-29 20:17:30 +0200

    add methods

--- src/main/System/Controller/UserController.java
+++ src/main/System/Controller/UserController.java
@@ -1,11 +1,23 @@
 package Controller;
 
+import Model.User;
+import Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private UserService userService;
+
+    public @ResponseBody
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
+
+    }
 
 }
--- src/main/System/Service/UserService.java
+++ src/main/System/Service/UserService.java
@@ -1,4 +1,18 @@
 package Service;
 
+import DAO.UserDao;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private UserDao userDao;
+
+    public List getAllUsers() {
+        return userDao.getAllUsers();
+
+    }
+
 }
