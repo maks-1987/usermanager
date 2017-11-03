20f988b2691d5a7a1b6da5467c418d225fc1ab78
maks-1987 <max.moderation@gmail.com>
2017-11-03 22:02:39 +0200

    505

--- src/main/System/Controller/UserController.java
+++ src/main/System/Controller/UserController.java
@@ -2,8 +2,10 @@ package Controller;
 
 import Model.User;
 import Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -12,8 +14,10 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public @ResponseBody
     List<User> getAllUsers() {
         return userService.getAllUsers();
--- src/main/System/Service/UserService.java
+++ src/main/System/Service/UserService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import DAO.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @Service
 public class UserService {
 
+    @Autowired
     private UserDao userDao;
 
     public List getAllUsers() {
